290f4079c178a607f404b4e7ecdf26e7c6990999
Ashu <ashutoshsarangi95@gmail.com>
2024-03-25 11:47:07 +0200

    Route Updae as per base

--- src/route/route.jsx
+++ src/route/route.jsx
@@ -9,7 +9,7 @@ import App from "../App";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/template-react-demo",
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
