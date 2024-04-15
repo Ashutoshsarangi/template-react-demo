0a03b4bad11c1b99d538c2fe8bec1f838ac2d2b2
Ashu <ashutoshsarangi95@gmail.com>
2024-04-15 14:05:29 +0300

    Added Few Members to contact

--- src/components/ContactCard/ContentCard.jsx
+++ src/components/ContactCard/ContentCard.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 
-const ContentCard = ({ imageSrc, name, phoneNo }) => {
+const ContentCard = ({ imageSrc, name, phoneNo, phoneTxt }) => {
   const { t } = useTranslation();
   return (
     <div className="m-3 p-3 border-lime-100 shadow">
@@ -13,10 +13,12 @@ const ContentCard = ({ imageSrc, name, phoneNo }) => {
         <img src={imageSrc} className="h-[250px] w-[250px] rounded-full" />
       </div>
       <div className="space-y-2">
-        <h2 className="text-2xl text-slate-600">
-          {name} ({phoneNo})
+        <h2 className="text-2xl text-slate-600 dark:text-slate-100">
+          {name} ({phoneTxt})
         </h2>
-        <p className="mb-3 text-slate-500">{t("feel_free_reach_out_us")}</p>
+        <p className="mb-3 text-slate-500 dark:text-slate-100">
+          {t("feel_free_reach_out_us")}
+        </p>
       </div>
     </div>
   );
