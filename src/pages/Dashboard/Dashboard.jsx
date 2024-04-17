c9d247e6d103249145605680de223f2b3f949711
Ashu <ashutoshsarangi95@gmail.com>
2024-04-17 21:56:31 +0300

    Added React gauge chart added Stages of development

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,30 +4,26 @@ import AppWidget from "../../components/AppWidget/AppWidget";
 import { useProgressiveImg } from "../../hooks/useProgressiveImg";
 import performanceLow from "../../assets/performance/performance_low.png";
 import performanceHigh from "../../assets/performance/performance_high.png";
+import Stage from "../../assets/stage.png";
+import StageMini from "../../assets/stage_mini.png";
 import { ourFeatures, ourServices } from "./Dashboard.constants";
 
 const Dashboard = ({ showPanel, setShowPanel }) => {
   const { t } = useTranslation();
   const [src, { blur }] = useProgressiveImg(performanceLow, performanceHigh);
+  const [srcStage, { blurStage }] = useProgressiveImg(StageMini, Stage);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
-      <ServiceCard
-        t={t}
-        ourServices={ourServices}
-        title="services"
-        viewMoreLink="/offer"
-      />
       <div className="bg-white p-3 shadow m-1 mb-3 flex justify-between flex-col dark:bg-slate-700">
         <img
-          src={src}
-          alt="This is related to performance of this website"
+          src={srcStage}
+          alt="This is related to State of our Service"
           style={{
-            filter: blur ? "blur(20px)" : "none",
-            transition: blur ? "none" : "filter 0.3s ease-out",
+            filter: blurStage ? "blur(20px)" : "none",
+            transition: blurStage ? "none" : "filter 0.3s ease-out",
           }}
         />
       </div>
-
       <AppWidget
         t={t}
         title="web_Applications"
@@ -36,6 +32,13 @@ const Dashboard = ({ showPanel, setShowPanel }) => {
         icon="chrome"
         noOfApps="10"
       />
+      <ServiceCard
+        t={t}
+        ourServices={ourServices}
+        title="services"
+        viewMoreLink="/offer"
+      />
+
       <AppWidget
         t={t}
         title="android_Applications"
@@ -50,6 +53,16 @@ const Dashboard = ({ showPanel, setShowPanel }) => {
         title="features"
         viewMoreLink="/feature"
       />
+      <div className="bg-white p-3 shadow m-1 mb-3 flex justify-between flex-col dark:bg-slate-700">
+        <img
+          src={src}
+          alt="This is related to performance of this website"
+          style={{
+            filter: blur ? "blur(20px)" : "none",
+            transition: blur ? "none" : "filter 0.3s ease-out",
+          }}
+        />
+      </div>
     </div>
   );
 };
