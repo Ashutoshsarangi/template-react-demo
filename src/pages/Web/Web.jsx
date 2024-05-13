7b079d6069a78ddb85de8513b7b315913adeadf4
sarangia <sarangia@amdocs.com>
2024-05-13 20:34:34 +0300

    Added 2 new projects, Paw season Spa and Nick Music also removed translation Feature for now.

--- src/pages/Web/Web.jsx
+++ src/pages/Web/Web.jsx
@@ -1,6 +1,10 @@
 import WebProjectCard from "../../components/WebProjectCard/WebProjectCard";
 import wedHighResImage from "../../assets/web/wedding/wed-high-resolution.png";
 import wedLowResImage from "../../assets/web/wedding/wed-low-resolution-min.png";
+import pawShopLow from "../../assets/web/pawSpa/pawShopLow.png";
+import pawShopHigh from "../../assets/web/pawSpa/pawShopHigh.png";
+import nickMusicLow from "../../assets/web/nickMusic/nickMusicLow.png";
+import nickMusicHigh from "../../assets/web/nickMusic/nickMusicHigh.png";
 
 const Web = (props) => {
   return (
@@ -11,6 +15,21 @@ const Web = (props) => {
         title="Digital Wedding Invitation Card"
         description="This is a digital wedding invitation card with different language features"
         projectLink="https://ashutoshsarangi.github.io/wedding-invitation-Assu/"
+      />
+      <WebProjectCard
+        lowResImage={pawShopLow}
+        highResImage={pawShopHigh}
+        title="Paw Season Spa"
+        description="This is a Paw Season Spa Website"
+        projectLink="https://paw-shop-demo.web.app/home"
+      />
+
+      <WebProjectCard
+        lowResImage={nickMusicLow}
+        highResImage={nickMusicHigh}
+        title="Nick Music"
+        description="This is a website is Made for Nick Music Institution."
+        projectLink="https://nickpanagi.com/"
       />
     </div>
   );
